78c66cb8795847e4e2d5911b8c1142adeb2933c9
Patrick Roelofs <contact@patrickroelofs.com>
2025-01-20 18:38:29 +0100

    fix: mobile view of components

--- src/blocks/TitleWithBlocks/titleWithBlocks.tsx
+++ src/blocks/TitleWithBlocks/titleWithBlocks.tsx
@@ -7,9 +7,9 @@ function TitleWithBlocks(props: TitleWithBlocksType): ReactElement {
   return (
     <div className="p-7 relative">
       <div className="container sm:grid grid-cols-4 gap-8 mb-12">
-        <div className="col-end-2 relative mb-12 sm:mb-auto">
+        <div className="col-end-2 relative mb-16 sm:mb-auto">
           <div className="sticky top-[65px] pt-4">
-            <h2 className="font-black text-3xl lg:text-7xl">{props.title}</h2>
+            <h2 className="lg:font-black text-3xl lg:text-7xl font-bold">{props.title}</h2>
             {props.showButton ? (
               <Link
                 href={props.buttonLink ?? ''}
