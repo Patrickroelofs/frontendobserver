5592364325fed944537f90ed8aaf94277ccdb793
Patrick Roelofs <contact@patrickroelofs.com>
2025-01-23 17:39:34 +0100

    fix: migrations scripts

--- migrations/20250123_163640.ts
+++ migrations/20250123_163640.ts
@@ -3,6 +3,9 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-vercel-postg
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
    ALTER TABLE "authors" ALTER COLUMN "short_bio" SET DEFAULT 'I''''m an author on this site.';`)
+
+  await db.execute(sql`
+    UPDATE "authors" SET "short_bio" = 'I''m an author on this site.' WHERE "short_bio" IS NULL;`)
 }
 
 export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
