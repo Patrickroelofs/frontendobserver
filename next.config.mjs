f5b1fa9ee59bbe570263bcb75a36ced7279c6942
Patrick Roelofs <contact@patrickroelofs.com>
2025-01-29 22:13:22 +0100

    Revert "debug: allow logging"
    This reverts commit d3aff4ca9d0a89a29e71a440494450ea386af239.

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  compiler: {
-    removeConsole: false,
-  },
+  // Your Next.js config here
 }
 
 export default withPayload(nextConfig)
